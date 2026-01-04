b0aedff188854a1fa47431b8462f4ca201e9a9e0
Micky <66727710+Mickeon@users.noreply.github.com>
2026-01-04 11:36:25 +0100

    Prepare for Serene Seasons removal
    This was discussed a bunch, concluding it's really mostly detrimental to the current modpack's goal

--- kubejs/startup_scripts/creative_tabs.js
+++ kubejs/startup_scripts/creative_tabs.js
@@ -307,11 +307,7 @@ remove_and_merge_into_tab("immersive_paintings:paintings", "minecraft:functional
 ])
 
 remove_tab("haventrowel:haventrowel_tab")
-remove_tab("sereneseasons:main")
 remove_tab("justhammers:creative_tab") // We are basically going to use only 4 Hammers from this.
-StartupEvents.modifyCreativeTab("minecraft:redstone_blocks", event => {
-	event.add("sereneseasons:season_sensor")
-})
 StartupEvents.modifyCreativeTab("ribbits:general", event => {
 	event.add("wanderer_ribbit:wanderer_ribbit_spawn_egg")
 	event.addAfter("ribbits:maraca", ["wanderer_ribbit:umbrellaleaf", "wanderer_ribbit:ribbit_map"])
@@ -319,7 +315,6 @@ StartupEvents.modifyCreativeTab("ribbits:general", event => {
 StartupEvents.modifyCreativeTab("minecraft:tools_and_utilities", event => {
 	event.remove("minecraft:bundle") // In case its experimental datapack is enabled.
 	event.add("minecraft:bundle")
-	event.add("sereneseasons:calendar")
 	event.add("haventrowel:trowel")
 	event.addAfter("minecraft:iron_axe", ["justhammers:iron_reinforced_hammer", "justhammers:iron_hammer"])
 	event.addAfter("minecraft:stone_axe", ["justhammers:stone_reinforced_hammer", "justhammers:stone_hammer"])
