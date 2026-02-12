c28cdce0a06b720bcbacecdc5f1c02497dac0f5d
Micky <66727710+Mickeon@users.noreply.github.com>
2026-02-12 22:41:22 +0100

    More sand crushing recipes

--- kubejs/server_scripts/recipes/misc.js
+++ kubejs/server_scripts/recipes/misc.js
@@ -308,6 +308,30 @@ ServerEvents.recipes(event => {
 	event.replaceInput({input: "minecraft:crafting_table"}, "minecraft:crafting_table", "#c:player_workstations/crafting_tables")
 	event.replaceInput({id: "minecraft:lectern"}, "minecraft:bookshelf", "#c:bookshelves")
 
+	// Part of the "Candle rebalancing act".
+	event.remove({id: "biomeswevegone:white_sand"}) // This recipe uses dye, which is somewhat gamebreaking for what we're going to do with this item.
+	event.recipes.create.milling(CreateItem.of(Item.of("biomeswevegone:white_sand")), Ingredient.of("minecraft:calcite"), 10 * SEC)
+	event.recipes.create.splashing([
+			CreateItem.of(Item.of("create:zinc_nugget"), 0.12),
+			CreateItem.of(Item.of("biomesoplenty:dead_branch"), 0.05)
+		],
+		Ingredient.of("biomeswevegone:white_sand").or("biomesoplenty:white_sand")
+	)
+	event.recipes.create.milling(CreateItem.of(Item.of("biomesoplenty:orange_sand")), Ingredient.of("arts_and_crafts:gypsum"), 10 * SEC)
+	event.recipes.create.splashing([
+			CreateItem.of(Item.of("minecraft:copper_nugget"), 0.12),
+			CreateItem.of(Item.of("biomesoplenty:dead_branch"), 0.05)
+		],
+		Ingredient.of("biomesoplenty:orange_sand")
+	)
+	event.recipes.create.milling(CreateItem.of(Item.of("biomesoplenty:black_sand")), Ingredient.of("minecraft:blackstone"), 10 * SEC)
+	event.recipes.create.splashing([
+			CreateItem.of(Item.of("minecraft:gold_nugget"), 0.12),
+			CreateItem.of(Item.of("biomesoplenty:dead_branch"), 0.05)
+		],
+		Ingredient.of("biomesoplenty:black_sand")
+	)
+
 	// TODO: Add recipes for (this modpack's) Pale Jacaranda and Redder Wood.
 	// event.shaped("biomesoplenty:stripped_jacaranda_log", ["DAD", "ADA", "DAD"], {D: "minecraft:white_dye", A: "biomeswevegone:stripped_jacaranda_log"})
 	// event.shaped("biomesoplenty:stripped_redwood_log", ["DAD", "ADA", "DAD"], {D: "minecraft:red_dye", A: "biomeswevegone:stripped_redwood_log"})
